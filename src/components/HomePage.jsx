9580d1cc2672db0aca82f752cc81e6a4b4674ed2
Pritomsaha27 <Pritom2001.saha@gmail.com>
2024-10-16 01:20:41 +0530

    updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import QRCode from "react-qr-code";
-
+import { BsChatSquareTextFill } from "react-icons/bs";
+import { FaLink } from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
 function HomePage() {
   return (
     <>
-      <p>home page</p>
-
-      <QRCode
-        className=""
-        size={300}
+    <div className="flex h-96  mt-20 container mx-auto">
+    <div className="w-1/2 border p-10 border-white">
+    <div className="flex justify-center gap-4">
+    <p className="flex items-center border rounded-lg w-20 justify-center gap-2 "><BsChatSquareTextFill />Text</p>
+    <p className="flex items-center border rounded-lg w-20 justify-center gap-2 "><FaLink />Link</p>
+    <p className="flex items-center border rounded-lg w-20 justify-center gap-2 "><AiFillMessage />SMS</p>
+    <p className="flex items-center border rounded-lg w-20 justify-center gap-2 "><MdEmail />Email</p>
+    </div>
+    </div>
+    <div className="w-1/2 border border-white">
+    <QRCode
+        className=" m-auto mt-20"
+        size={200}
         value={"https://pixelperfectapp.netlify.app/"}
-        viewBox={`0 0 256 256`}
       />
+    </div>
+    </div>
     </>
   );
 }
